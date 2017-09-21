a298c2cb3a238451093531962a83a2fa4d56941b
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-22 00:29:45 +0600

    Find Cylinder size average

--- Read And Write CSV file/csv file read.py
+++ Read And Write CSV file/csv file read.py
@@ -5,4 +5,23 @@ with open('mpg.csv') as csvfile:
 
  
 
-print sum(float(i['weight'] ) for i in mpg ) / len(mpg)
+cylinders = set( i['cylinders'] for i in mpg )
+cylinderAvg = list()
+
+
+for i in cylinders:
+
+	cnt = 0
+	val = 0 
+
+	for j in mpg:
+		if j['cylinders']==i:
+			cnt = 1 + cnt
+			val = val + float(i)
+
+    
+	cylinderAvg.append( (i, val/cnt ) )
+
+cylinderAvg.sort(key = lambda x : x[0] )
+
+print cylinderAvg
