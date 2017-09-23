d2f3059775c30ff15df80cbbb8b16f62c81b850d
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-23 11:38:21 +0600

    Numpy operation

--- Advance Python/Numpy/Numpy.py
+++ Advance Python/Numpy/Numpy.py
@@ -9,7 +9,7 @@ two_D_arrya.shape
 
 # reshape the array
 
-print two_D_arrya.reshape(1,4)
+#print two_D_arrya.reshape(1,4)
 
 # add one array with other . column number need to be equal
 
@@ -17,3 +17,19 @@ print two_D_arrya.reshape(1,4)
 print np.vstack( [two_D_arrya,2*two_D_arrya ] )
 # add horizontally 
 print np.hstack( [two_D_arrya,3*two_D_arrya ] )
+
+# more operation 
+
+# Transpose Matrix
+
+print two_D_arrya.T
+#Dot product 
+
+print two_D_arrya.dot(two_D_arrya)
+
+print two_D_arrya.sum()
+print two_D_arrya.max()
+print two_D_arrya.min()
+print two_D_arrya.mean() # average 
+print two_D_arrya.std() # standard  deviation. point distribution system
+print two_D_arrya.argmax()
