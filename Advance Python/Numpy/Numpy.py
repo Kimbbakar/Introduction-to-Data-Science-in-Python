fd35065cd82fed1cf7bc33ec33d9f8112664953f
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-23 11:29:45 +0600

    Add one array with others

--- Advance Python/Numpy/Numpy.py
+++ Advance Python/Numpy/Numpy.py
@@ -10,3 +10,10 @@ two_D_arrya.shape
 # reshape the array
 
 print two_D_arrya.reshape(1,4)
+
+# add one array with other . column number need to be equal
+
+# add vertically
+print np.vstack( [two_D_arrya,2*two_D_arrya ] )
+# add horizontally 
+print np.hstack( [two_D_arrya,3*two_D_arrya ] )
