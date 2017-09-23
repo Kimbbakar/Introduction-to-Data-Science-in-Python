e410a5aee74df14772652acc3563a1ec28079e51
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-23 11:16:49 +0600

    generate ID using list 4D list comprehension

--- Advance Python/Lamda and List comprehesions/List Comprehension.py
+++ Advance Python/Lamda and List comprehesions/List Comprehension.py
@@ -8,7 +8,7 @@ for number in range(0,1000):
 	if number % 2 == 0:
 		my_list.append(number)
 
-print my_list
+#print my_list
 
 # option 2 : using list comprehension
 
@@ -16,10 +16,10 @@ my_list = [ number for number in range(1000) if number % 2 == 0 ]
 
 
 
-print my_list
+#print my_list
 
 
-# task
+# task 1
 
 # convert this function to list comprehension 
 def times_tables():
@@ -32,4 +32,14 @@ def times_tables():
 lst = [ i * j for i in range(10) for j in range(10) ]
  
 
-print lst
+#print lst
+
+# task 2
+
+
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+answer = list( i  + j + k + l for i in lowercase for j in lowercase  for k in digits  for l in digits  )
+ 
+print answer 
