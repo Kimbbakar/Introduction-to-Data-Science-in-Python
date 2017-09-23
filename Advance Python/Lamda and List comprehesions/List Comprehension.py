d39641f6ab7137f18ea06dfc5dd51bf93b064365
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-23 11:00:40 +0600

    Convert a function using list comprehension

--- Advance Python/Lamda and List comprehesions/List Comprehension.py
+++ Advance Python/Lamda and List comprehesions/List Comprehension.py
@@ -17,3 +17,19 @@ my_list = [ number for number in range(1000) if number % 2 == 0 ]
 
 
 print my_list
+
+
+# task
+
+# convert this function to list comprehension 
+def times_tables():
+    lst = []
+    for i in range(10):
+        for j in range (10):
+            lst.append(i*j)
+    return lst
+
+lst = [ i * j for i in range(10) for j in range(10) ]
+ 
+
+print lst
