a4d4d0d3de327ac4654ec14aea6b8f7a6d8b94cd
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-24 11:31:50 +0600

    Different type of pandas.Series call

--- Data tools/Panda/Pandas.py
+++ Data tools/Panda/Pandas.py
@@ -1,9 +1,24 @@
 import pandas as pd 
 
+# Auto indexing 
+
 animal = ['Tiger','Bear','Moose']
 
 print pd.Series(animal)
 
+# indexing from dictionary 
+
 sports = { 'Archery':'Bhutan', 'Golf':'Scotland','Sumo':'Japan','Taekwondo':'South Korea' }
 
-print sports
+print pd.Series(sports)
+
+# explicit mention for index 
+
+
+print pd.Series( ['a','b','c'], index = [ 'x','y','z' ] )
+
+# mention in dictionary, but not mention in explicit index
+
+name = { 'a':1,'b':2,'c':3,'d':4 }
+
+print pd.Series(name,index=['a','c','d','z' ] )
