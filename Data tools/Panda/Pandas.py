3b89f92ba417da3f44e448a2b5014232b666af1e
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-24 11:16:31 +0600

    import Pandas

---
+++ Data tools/Panda/Pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+animal = ['Tiger','Bear','Moose']
+
+print pd.Series(animal)
+
+sports = { 'Archery':'Bhutan', 'Golf':'Scotland','Sumo':'Japan','Taekwondo':'South Korea' }
+
+print sports
