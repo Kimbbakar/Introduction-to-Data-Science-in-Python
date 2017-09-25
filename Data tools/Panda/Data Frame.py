f62876a2a8feb461dacb81448410ef7a8494add9
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-25 20:45:10 +0600

    Extract infrom from Data frame data structure

--- Data tools/Panda/Data Frame.py
+++ Data tools/Panda/Data Frame.py
@@ -1,2 +1,34 @@
 import pandas as pd 
 
+# Use Dictionary as e data type for 
+
+purchase_1 = pd.Series({
+		'Name' : 'Chris',
+		'Item Purchased': 'Dog Food',
+		'Cost': 22.50
+	})
+
+purchase_2 = pd.Series ( {
+		'Name' : 'Osman',
+		'Item Purchased': 'abc',
+		'Cost': 12.0
+	})
+
+purchase_3 = pd.Series ( {
+		'Name' : 'mak',
+		'Item Purchased': 'zzz',
+		'Cost': 1.0
+	})
+
+df = pd.DataFrame( [purchase_1,purchase_2,purchase_3 ], index = ['Store 1','Store 1','Store 3'] )
+
+print df 
+
+
+
+print df ['Cost']
+print df ['Item Purchased']
+ 
+#print df.T
+
+print df.loc[ ['Store 1']  , [ 'Name', "Cost" ] ] 
