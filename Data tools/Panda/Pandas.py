f2eaf5a1648c0206ac3c121190c55f926a81ec34
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-25 18:00:09 +0600

    Different way to query using pandas

--- Data tools/Panda/Pandas.py
+++ Data tools/Panda/Pandas.py
@@ -22,3 +22,31 @@ print pd.Series( ['a','b','c'], index = [ 'x','y','z' ] )
 name = { 'a':1,'b':2,'c':3,'d':4 }
 
 print pd.Series(name,index=['a','c','d','z' ] )
+
+print ('-' *50)
+
+#Querying A Series
+
+sports = { 'Archery':'Bhutan', 'Golf':'Scotland','Sumo':'Japan','Taekwondo':'South Korea' }
+S = pd.Series(sports)
+
+# query by index 
+print S[2]
+print S.iloc[2]
+
+
+# query by range
+
+print S[:3]
+
+# query like a limited dictionary
+
+# some time calling by index create ambiguity. 
+# That is why we need to use iloc and loc.
+# iloc = find the value using row number
+# loc = find the value using tag 
+
+
+print S.loc['Taekwondo']
+print S['Golf']
+
