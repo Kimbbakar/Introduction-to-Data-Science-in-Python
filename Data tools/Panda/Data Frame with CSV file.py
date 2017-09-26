f502c2027f1e212af6c76338f1ee4b279f2076e8
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-26 21:11:00 +0600

    Import csv file and set index and header.

--- Data tools/Panda/Data Frame with CSV file.py
+++ Data tools/Panda/Data Frame with CSV file.py
@@ -1,2 +1,15 @@
 import pandas as pd 
 
+# import CSV file
+
+# to make a column as  index value use index_col = 'column number'
+# to make a row as  a header value , skip all the row until that one. skiprow = 'row number'
+
+df = pd.read_csv('mpg.csv',skiprows = 1 )
+
+
+
+print df['mpg']
+
+print df.loc[18.0]
+print df.head()
