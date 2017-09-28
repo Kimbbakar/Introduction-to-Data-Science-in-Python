44ca15ec1068a3e1575a88094a4fa1bb680a9835
Kimbbakar <chowdhuryosman04@gmail.com>
2017-09-28 10:55:15 +0600

    Query with boolean mask

---
+++ Data tools/Panda/Query with Data Frame.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+
+
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.50})
+purchase_2 = pd.Series({'Name': 'Kevyn',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.50})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.00})
+
+df = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+ 
+
+# Use of bool masking
+
+
+
+#printing the name who cost more than $3
+print df[ df['Cost'] >3.00] ['Name']
+
+print ('-' *50)
+
+
+# printing the name and Item who cost more than $3 but less than $6
+ 
+
+print df.loc[ : ,['Name' , 'Item Purchased' ] ][( df['Cost'] >3.00) &  ( df['Cost'] <6.00) ]
