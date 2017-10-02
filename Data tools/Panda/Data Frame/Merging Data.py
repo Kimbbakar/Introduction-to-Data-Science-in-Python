b90c6d2f2dca4a2b7cd644b0558fe658e8752202
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-02 20:30:38 +0600

    Outer join

--- Data tools/Panda/Data Frame/Merging Data.py
+++ Data tools/Panda/Data Frame/Merging Data.py
@@ -10,6 +10,9 @@ student_df = pd.DataFrame([{'Name': 'James', 'School': 'Business'},
                            {'Name': 'Mike', 'School': 'Law'},
                            {'Name': 'Sally', 'School': 'Engineering'}])
 
-
 student_df = student_df.set_index('Name')
- 
+
+
+outer_join = pd.merge(staff_df,student_df,how = 'outer',left_index = True, right_index = True );
+
+print outer_join
