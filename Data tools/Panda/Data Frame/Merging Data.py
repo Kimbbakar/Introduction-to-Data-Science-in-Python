d8984756039a2333b75b839bf8f4293520019a1b
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-02 20:19:48 +0600

    added 2 df. Student and Stuff

---
+++ Data tools/Panda/Data Frame/Merging Data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+staff_df = pd.DataFrame([{'Name': 'Kelly', 'Role': 'Director of HR'},
+                         {'Name': 'Sally', 'Role': 'Course liasion'},
+                         {'Name': 'James', 'Role': 'Grader'}])
+
+staff_df = staff_df.set_index('Name')
+
+student_df = pd.DataFrame([{'Name': 'James', 'School': 'Business'},
+                           {'Name': 'Mike', 'School': 'Law'},
+                           {'Name': 'Sally', 'School': 'Engineering'}])
+
+
+student_df = student_df.set_index('Name')
+ 
